0bf4325956b0fae8694335acc4648942e62e53dc
Luana Orell Padilla <lorellpad@gmail.com>
2018-12-03 14:47:44 +0000

    Endpoint post course

--- evaBACKEND/Data/DummyData.cs
+++ evaBACKEND/Data/DummyData.cs
@@ -37,8 +37,69 @@ namespace evaBACKEND.Data
 				await userManager.AddToRoleAsync(adminUser, "Admin");
 			}
 
+			var teacherUser = new AppUser();
+			teacherUser.FirstName = "Pablo";
+			teacherUser.LastName = "Gonzales";
+			teacherUser.Email = "pablo.gz@mail.com";
+			teacherUser.UserName = "pablo.gz@mail.com";
+
+			if (await userManager.FindByEmailAsync(teacherUser.Email) == null)
+			{
+				await userManager.CreateAsync(teacherUser, "Control123!");
+				await userManager.AddToRoleAsync(teacherUser, "Docente");
+			}
+
+			var javaCourse = new Course();
+			javaCourse.Name = "Java para principiantes";
+			javaCourse.Description = "Conceptos basicos de uno de lenguajes mas utilizados al rededor del mundo," +
+				"en el cuerso aprenderas todo lo relicionado con POO";
+			javaCourse.StartDate = DateTime.Today;
+			javaCourse.Teacher = teacherUser;
+
+			var existCourse = context.Courses.Where(c => c.Name.Equals(javaCourse.Name)).Count();
+
+			if (existCourse == 0)
+			{
+				context.Courses.Add(javaCourse);
+				await context.SaveChangesAsync();
+			}
+
+			var csharpCourse = new Course();
+			csharpCourse.Name = "Programación en C#";
+			csharpCourse.Description = "En este curso aprenderás a programar desde cero. No necesitarás " +
+				"tener conocimientos previos en programación para aprender." +
+				"Empezaremos por lo más básico, desde instalar el compilador donde trabajar, pasando por la " +
+				"realización de pequeños programas de consola, hasta la programación orientada a objetos.";
+			csharpCourse.Teacher = teacherUser;
+			csharpCourse.StartDate = new DateTime(2019, 1, 3, 16, 5, 7, 123);
+
+			existCourse = context.Courses.Where(c => c.Name.Equals(csharpCourse.Name)).Count();
+
+			if (existCourse == 0) {
+				context.Courses.Add(csharpCourse);
+				await context.SaveChangesAsync();
+			}
+			
+
+			var student = new AppUser();
+			student.FirstName = "Andrea";
+			student.LastName = "Panozo";
+			student.Email = "andy24@mail.com";
+			student.UserName = "andy24@mail.com";
+
+			if (await userManager.FindByEmailAsync(student.Email) == null)
+			{
+				await userManager.CreateAsync(student, "Control123!");
+				await userManager.AddToRoleAsync(student, "Estudiante");
+			}
+
+			var courseUser = new CourseUser();
+			courseUser.CourseId = javaCourse.CourseId;
+			courseUser.Id = student.Id;
+
+			context.CourseUsers.Add(courseUser);
+			await context.SaveChangesAsync();
 		}
 	}
-
-			
+		
 }
